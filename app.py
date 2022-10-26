b0908b406ed82773b19a4aa7e8f67a780661b4ad
Laurenz Dilba <laurenz.dilba@hotmail.de>
2022-10-26 09:33:52 +0200

    Added Upload functionality

--- app.py
+++ app.py
@@ -1,7 +1,27 @@
-from flask import Flask, Blueprint, request
+import os
+
+import redis
+from flask import Flask, Blueprint, request, flash, redirect, url_for, session
+from flask_session import Session
 from api import response_generator as r
+import json
+
+ALLOWED_EXTENSIONS = {'txt', 'json'}
 
 app = Flask(__name__)
+
+app.secret_key = '|9vpN<gctB6fBvv5MqV5|XMAOE0Qu3'
+
+app.config['SESSION_TYPE'] = 'redis'
+app.config['SESSION_PERMANENT'] = False
+app.config['SESSION_REDIS'] = redis.from_url('redis://localhost:6379')
+
+sess = Session(app)
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route('/', methods=["GET"])
@@ -9,5 +29,35 @@ def index():
     return r.respond({"test": True})
 
 
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        return r.respond({"success": False, "status": "No File Part"}, 400)
+    file = request.files['file']
+    # If the user does not select a file, the browser submits an
+    # empty file without a filename.
+    if file.filename == '':
+        return r.respond({"success": False, "status": "No Selected File"}, 400)
+    if file and allowed_file(file.filename):
+        file_content = file.stream.read().decode("utf-8")
+        print(file_content)
+        session['file'] = file_content
+    else:
+        return r.respond({"success": False, "status": "Filetype Not Allowed"}, 400)
+
+    return r.respond({"file": session['file']})
+
+
+@app.route('/transform', methods=['POST'])
+def transform():
+    transform_json = request.json
+
+    if session['file'] != '':
+        file = str(session['file'])
+        file = file.split(transform_json['seperator'])
+        return r.respond({"fileParts": file})
+
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=3210, use_reloader=False)
+    app.run(host="0.0.0.0", port=80, use_reloader=False)
---
+++ readme.md
@@ -0,0 +1,4 @@
+boot redis storage:  
+```bash
+$ sudo docker run -p 6379:6379 --name redis_cache -d redis 
+```
