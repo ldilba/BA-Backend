5bd3b2e03404cb8931e749f00d420e1fac75bad1
Laurenz Dilba <laurenz.dilba@hotmail.de>
2022-11-04 14:50:35 +0100

    Connection nach Request schließen

--- broker/message_broker.py
+++ broker/message_broker.py
@@ -4,13 +4,15 @@ import pika
 
 credentials = pika.PlainCredentials('guest', 'guest')
 rabbit_host = os.environ.get('RABBIT_HOST')
-connection = pika.BlockingConnection(pika.ConnectionParameters(rabbit_host, 5672, '/', credentials))
-channel = connection.channel()
 
 
 def produce(uid, service, message):
+    connection = pika.BlockingConnection(pika.ConnectionParameters(rabbit_host, 5672, '/', credentials))
+    channel = connection.channel()
     channel.queue_declare(queue=service)
 
     channel.basic_publish(exchange='',
                           routing_key=service,
                           body=f'{{"uid": "{uid}", "service": "{service}", "message": "{message}"}}'.encode('utf-8'))
+    channel.close()
+    connection.close()
